eb877d69b59ea224dda368330ae5afc8a4033289
Jakub Pniak <jpniak@o2.pl>
2020-05-05 15:00:16 +0200

    Finish submodule 5.3

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 'use strict';
 
 function titleClickHandler(event){
-  const clickedElement = this;
-  console.log('Link was clicked!');
+    event.preventDefault();
+    const clickedElement = this;
+    console.log('Link was clicked!');
 
   /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-const activeLinks = document.querySelectorAll('.titles a.active');
-
-for(let activeLink of activeLinks){
+    for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
-}
+    }
 
   /* add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
@@ -19,22 +19,29 @@ for(let activeLink of activeLinks){
 
   /* remove class 'active' from all articles */
     
-const activeArticles = document.querySelectorAll('.posts article.active');
+    const activeArticles = document.querySelectorAll('.posts article.active');
 
-for(let activeArticle of activeArticles){
+    for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
 }
 
 
   /* get 'href' attribute from the clicked link */
 
+    const articleSelector = clickedElement.getAttribute('href')
+    console.log('Clicked selector is:', articleSelector)
+
   /* find the correct article using the selector (value of 'href' attribute) */
-
+    const targetArticle = document.querySelector(articleSelector)
+    console.log('Chosen article is:', targetArticle)
+    
   /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
+    console.log('Active article is:', targetArticle);
 }
 
-const links = document.querySelectorAll('.titles a');
+    const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-}
+    for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+    }
