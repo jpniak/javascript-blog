9b9f9d59184f2fc70e9364fc37673cc74737fe6d
Jakub Pniak <jpniak@o2.pl>
2020-05-06 13:37:53 +0200

    Finish submodule 5.4

--- js/script.js
+++ js/script.js
@@ -41,7 +41,55 @@ function titleClickHandler(event){
 }
 
     const links = document.querySelectorAll('.titles a');
+    console.log(links);
 
     for(let link of links){
     link.addEventListener('click', titleClickHandler);
     }
+
+//Second part - submodule 5.4
+
+    const optArticleSelector = '.post', optTitleSelector = '.post-title', optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+    console.log('function generateTitleLinks has started')
+
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector)
+    console.log('znaleziono liste linkow')
+    titleList.innerHTML = '';
+    console.log('wyczyszczono liste linkow')
+    
+    /* find all the articles and save them to variable: articles */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    
+    let html = '';
+    
+    /* for each article */
+    for(let article of articles){ // Co ta pętla ma robić?, bo już się zgubiłem... ah, chyba wiem - te punkty z komentarzy :-)
+    
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+    console.log('pobrano atrybuty ID');
+    
+    
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log('odczytano tytul artykulu');
+    
+    /* get the title from the title element */
+    //wg mnie to powyzej tez TO robi...
+    
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+        
+    /* insert link into html variable */
+    html = html + linkHTML;        
+    }
+    
+    titleList.innerHTML = html;
+}
+
+generateTitleLinks();
